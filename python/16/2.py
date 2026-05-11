c97d91fc0e6e8cac1036d0bc2ba2dce29b897ffb
Dmitriy <dmitriy.hellwig@gmail.com>
2026-05-11 22:29:49 +0200

    p 16

---
+++ python/16/2.py
@@ -0,0 +1,64 @@
+""" 02 Правильные скобки
+
+Напишите программу, которая
+- принимает строку, содержащую любые виды скобок
+    — круглые (),
+    — квадратные []
+    — и фигурные {}.
+
+Необходимо проверить, правильно ли они расставлены.
+Скобки считаются правильно расставленными, если:
+    Каждая открывающая скобка имеет соответствующую закрывающую.
+    Скобки закрываются в правильном порядке.
+
+Пример данных:
+({[]}): True
+({[}]): False
+([]{}): True
+: True
+{[()()]}: True
+([)]: False
+{[)(()]}: False
+({[): False
+"""
+from collections import deque
+
+LEFT_BRACKETS = '([{'
+RIGHT_BRACKETS = ')]}'
+
+def check_brackets(string):
+    stack = deque()
+
+    for char in string:
+        if char in LEFT_BRACKETS:
+            stack.append(char)
+
+        else:
+            if not stack:
+                return False
+
+            i = RIGHT_BRACKETS.find(char)
+
+            if i < 0:
+                return False
+            if stack.pop() != LEFT_BRACKETS[i]:
+                return False
+
+    return not stack
+
+
+test_cases = [
+    "({[]})",
+    "({[}])",
+    "([]{})",
+    "",
+    "{[()()]}",
+    "([)]",
+    "{[)(()]}",
+    "({[)",
+]
+
+for test_case in test_cases:
+    print(f"{test_case}: {check_brackets(test_case)}")
+
+
